090afb57012ceaee7ad1d6846ebdb5bbc808b424
GawrGura <lixunsam@gmail.com>
2022-03-02 09:33:35 +0800

    axios 拦截器

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import "ant-design-vue/dist/antd.css";
 import * as Icons from "@ant-design/icons-vue";
 import axios from "axios";
 
-axios.defaults.baseURL=process.env.VUE_APP_API
+axios.defaults.baseURL = process.env.VUE_APP_API;
 
 const app = createApp(App);
 app.use(store).use(Antd).use(router).mount("#app");
@@ -17,3 +17,26 @@ const icons: any = Icons;
 for (const i in icons) {
   app.component(i, icons[i]);
 }
+
+/**
+ * axios拦截器
+ */
+axios.interceptors.request.use(
+  function (config) {
+    console.log("请求参数：", config);
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+axios.interceptors.response.use(
+  function (response) {
+    console.log("返回结果：", response);
+    return response;
+  },
+  (error) => {
+    console.log("返回错误：", error);
+    return Promise.reject(error);
+  }
+);
