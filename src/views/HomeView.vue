c0e7f0b83414e7d8a39dbac93fce18245ede1a51
GawrGura <lixunsam@gmail.com>
2022-03-02 09:20:21 +0800

    add proxy

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,10 +14,10 @@
               subnav 1
             </span>
           </template>
-          <a-menu-item key="1">Java集合</a-menu-item>
-          <a-menu-item key="2">Vue集合</a-menu-item>
-          <a-menu-item key="3">mysql集合</a-menu-item>
-          <a-menu-item key="4">架构师之路</a-menu-item>
+          <a-menu-item key="1">option11111</a-menu-item>
+          <a-menu-item key="2">option2</a-menu-item>
+          <a-menu-item key="3">option3</a-menu-item>
+          <a-menu-item key="4">option4</a-menu-item>
         </a-sub-menu>
         <a-sub-menu key="sub2">
           <template #title>
@@ -53,39 +53,71 @@
         minHeight: '280px',
       }"
     >
-      Content
-      <div id="my"></div>
-      {{ ebooks }}
-      {{ mybook }}
+      <!-- {{ ebooks }} -->
+      <a-list
+        item-layout="vertical"
+        size="large"
+        :grid="{ gutter: 20, column: 3 }"
+        :pagination="pagination"
+        :data-source="ebooks"
+      >
+        <template #renderItem="{ item }">
+          <a-list-item key="item.name">
+            <template #actions>
+              <span v-for="{ type, text } in actions" :key="type">
+                <component v-bind:is="type" style="margin-right: 8px" />
+                {{ text }}
+              </span>
+            </template>
+            <a-list-item-meta :description="item.description">
+              <template #title>
+                <a :href="item.href">{{ item.name }}</a>
+              </template>
+              <template #avatar><a-avatar :src="item.cover" /></template>
+            </a-list-item-meta>
+          </a-list-item>
+        </template>
+      </a-list>
     </a-layout-content>
   </a-layout>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, onMounted, ref } from "vue";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 import axios from "axios";
 
 export default defineComponent({
-  // name: 'Home-View',
-  name: "Home-View",
+  name: "HomeView",
   components: {
     HelloWorld,
   },
-  data() {
-    return {
-      mybook: "Summer Pockets",
-    };
-  },
   setup() {
-    // console.log(props);
-    let ebooks = {};
-    axios.get("http://localhost:8088/ebook/findAllEbook").then((response) => {
-      console.log(response);
-      ebooks = response.data;
-      // this.data = response.data;
+    console.log("setup");
+    const ebooks = ref();
+    onMounted(() => {
+      axios.get("/ebook/findAllEbook").then((response) => {
+        console.log(response);
+        const data = response.data;
+        ebooks.value = data.data;
+      });
     });
-    return ebooks;
+    const pagination = {
+      onChange: (page: number) => {
+        console.log(page);
+      },
+      pageSize: 3,
+    };
+    const actions: Record<string, string>[] = [
+      { type: "StarOutlined", text: "156" },
+      { type: "LikeOutlined", text: "156" },
+      { type: "MessageOutlined", text: "2" },
+    ];
+    return {
+      ebooks,
+      pagination,
+      actions,
+    };
   },
 });
 </script>
