345a08774f0372003e4800dacac9848a9e12db1f
GawrGura <lixunsam@gmail.com>
2022-03-28 15:51:15 +0800

    category

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,42 +6,19 @@
         v-model:selectedKeys="selectedKeys2"
         v-model:openKeys="openKeys"
         :style="{ height: '100%', borderRight: 0 }"
+        @click="handleClick"
       >
-        <a-sub-menu key="sub1">
-          <template #title>
-            <span>
-              <user-outlined />
-              subnav 1
-            </span>
+        <a-menu-item key="welcome">
+          <MailOutlined />
+          <span>欢迎</span>
+        </a-menu-item>
+        <a-sub-menu v-for="item in level1" :key="item.id">
+          <template v-slot:title>
+            <span><user-outlined />{{ item.name }}</span>
           </template>
-          <a-menu-item key="1">option11111</a-menu-item>
-          <a-menu-item key="2">option2</a-menu-item>
-          <a-menu-item key="3">option3</a-menu-item>
-          <a-menu-item key="4">option4</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub2">
-          <template #title>
-            <span>
-              <laptop-outlined />
-              subnav 2
-            </span>
-          </template>
-          <a-menu-item key="5">option5</a-menu-item>
-          <a-menu-item key="6">option6</a-menu-item>
-          <a-menu-item key="7">option7</a-menu-item>
-          <a-menu-item key="8">option8</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub3">
-          <template #title>
-            <span>
-              <notification-outlined />
-              subnav 3
-            </span>
-          </template>
-          <a-menu-item key="9">option9</a-menu-item>
-          <a-menu-item key="10">option10</a-menu-item>
-          <a-menu-item key="11">option11</a-menu-item>
-          <a-menu-item key="12">option12</a-menu-item>
+          <a-menu-item v-for="child in item.children" :key="child.id">
+            <MailOutlined /><span>{{ child.name }}</span>
+          </a-menu-item>
         </a-sub-menu>
       </a-menu>
     </a-layout-sider>
@@ -53,8 +30,12 @@
         minHeight: '280px',
       }"
     >
+      <div class="welcome" v-show="isShowWelcome">
+        <h1>欢迎来到我的知识库</h1>
+      </div>
       <!-- {{ ebooks }} -->
       <a-list
+        v-show="!isShowWelcome"
         item-layout="vertical"
         size="large"
         :grid="{ gutter: 20, column: 3 }"
@@ -86,6 +67,8 @@
 import { defineComponent, onMounted, ref } from "vue";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 import axios from "axios";
+import { message } from "ant-design-vue";
+import { Tool } from "@/utils/tool";
 
 export default defineComponent({
   name: "HomeView",
@@ -95,6 +78,7 @@ export default defineComponent({
   setup() {
     console.log("setup");
     const ebooks = ref();
+
     const pagination = {
       onChange: (pageNum: number) => {
         axios
@@ -114,12 +98,69 @@ export default defineComponent({
       total: "",
       hideOnSinglePage: true,
     };
+
+    const level1 = ref();
+    let categorys: any;
+    /**
+     * 查询所有分类
+     **/
+    const handleQueryCategory = () => {
+      axios.get("/category/all").then((response) => {
+        const { data } = response;
+        if (data.code === 200) {
+          categorys = data.data;
+          console.log("原始数组：", categorys);
+          level1.value = [];
+          level1.value = Tool.array2Tree(categorys, 0);
+          console.log("树形结构：", level1.value);
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
+    const isShowWelcome = ref(true);
+    let category2Id = 0;
+
+    const handleQueryEbook = () => {
+      axios
+        .get("/ebook/findAllEbook", {
+          params: {
+            pageNum: 1,
+            pageSize: 1000,
+            category2Id: category2Id,
+          },
+        })
+        .then((response) => {
+          const { data } = response.data;
+          console.log("data", data);
+          ebooks.value = data.list;
+
+          pagination.pageSize = data.pageSize;
+          pagination.total = data.total;
+          pagination.current = data.pageNum;
+        });
+    };
+
+    const handleClick = (value: any) => {
+      console.log("首页左边分类被点击啦啦啦", value);
+      if (value.key === "welcoome") {
+        isShowWelcome.value = true;
+      } else {
+        category2Id = value.key;
+        isShowWelcome.value = false;
+        handleQueryEbook();
+      }
+    };
+
     const actions: Record<string, string>[] = [
       { type: "StarOutlined", text: "156" },
       { type: "LikeOutlined", text: "156" },
       { type: "MessageOutlined", text: "2" },
     ];
     onMounted(() => {
+      handleQueryCategory();
+
       axios
         .get("/ebook/findAllEbook", { params: { pageNum: 1, pageSize: 9 } })
         .then((response) => {
@@ -134,6 +175,11 @@ export default defineComponent({
       ebooks,
       pagination,
       actions,
+
+      handleClick,
+      level1,
+
+      isShowWelcome,
     };
   },
 });
